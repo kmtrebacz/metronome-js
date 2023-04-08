c14826dba2368df727810c21cbe28407d74b81e3
Kacper <kacperkapitrebacz@gmail.com>
2023-04-08 15:49:19 +0200

    js code create red dots in amount of #dots input

--- src/mentronome.js
+++ src/mentronome.js
@@ -6,3 +6,21 @@ inputRange.addEventListener('input', () => {
   const rangeValue = inputRange.value
   bmp.innerHTML = `${rangeValue} BPM`
 })
+
+// create red dots in amount of #dots input
+document.querySelector('#dots').addEventListener('change', () => {
+  const dotsAmount = document.querySelector('#dots').value;
+  const tempoPointers = document.getElementById('tempo-pointers');
+
+  // Clear existing tempo pointers
+  tempoPointers.innerHTML = '';
+
+  for (let i = 1; i <= dotsAmount; i++){
+    const newDot = document.createElement('div');
+    newDot.setAttribute('class', 'tempo-pointer');
+    console.log(newDot);
+    console.log(i);
+    tempoPointers.appendChild(newDot);
+  }
+});
+
