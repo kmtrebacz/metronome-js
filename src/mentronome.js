4ea34367bef6145644671f924bf723af6485c11b
Kacper <kacperkapitrebacz@gmail.com>
2023-04-10 22:42:25 +0200

    Change in html and css

--- src/mentronome.js
+++ src/mentronome.js
@@ -1,27 +1,37 @@
 // reading bmp value from range input
-let inputRange  = document.querySelector('input[type=range]')
-let bmp = document.querySelector('#bmp')
+let inputRange = document.querySelector('input[type=range]')
+let bpm = document.querySelector('#bmp')
 
 inputRange.addEventListener('input', () => {
   const rangeValue = inputRange.value
-  bmp.innerHTML = `${rangeValue} BPM`
+  bpm.innerHTML = `${rangeValue} BPM`
 })
 
 // create red dots in amount of #dots input
 document.querySelector('#dots').addEventListener('change', () => {
-  const dotsAmount = document.querySelector('#dots').value;
-  const tempoPointers = document.getElementById('tempo-pointers');
+  const dotsAmount = document.querySelector('#dots').value
+  const tempoPointers = document.getElementById('tempo-pointers')
 
   // Clear existing tempo pointers
-  tempoPointers.innerHTML = '';
+  tempoPointers.innerHTML = ''
 
   for (let i = 1; i <= dotsAmount; i++){
-    const newDot = document.createElement('div');
-    newDot.setAttribute('class', 'tempo-pointer');
-    newDot.setAttribute('id', i);
-    console.log(newDot);
-    console.log(i);
-    tempoPointers.appendChild(newDot);
+    const newDot = document.createElement('div')
+    newDot.setAttribute('class', 'tempo-pointer')
+    newDot.setAttribute('id', i)
+    console.log(newDot)
+    console.log(i)
+    tempoPointers.appendChild(newDot)
   }
-});
+})
+
+document.querySelector('#start').addEventListener('click', () => {
+for (let x = 1; x <= dotsAmount; x++) {
+  for (let y = 1; y <= clicksAmount; y++) {
+    setTimeout(() => {
+      console.log(`${x}, ${y}`);
+    }, y * 500);
+  }
+}
+})
 
